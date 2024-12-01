cd80245e8b42be60ec795f49268fc98029d41c82
TuLitFav <edutomate40@gmail.com>
2024-12-01 14:42:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,4 @@ document.getElementById('characterForm').addEventListener('submit', function(eve
     const basicInfo = document.getElementById('basicInfo').value;
     const imageInput = document.getElementById('imageInput').files[0];
 
-    const characterList = document.getElementById('characterList');
-    const listItem = document.createElement('li');
-    listItem.textContent = `${firstName} ${lastName} - ${basicInfo}`;
-
-    // Crear un contenedor para la imagen
-    if (imageInput) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const img = document.createElement('img');
-            img.src
+    const characterList = document.getElementById('characterList
